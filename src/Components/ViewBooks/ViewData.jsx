b212b5392fa0f1b1bf2f5e15af0313b84d85478d
BIKRAMNAHAK <bikramnahak9714@gmail.com>
2024-07-14 18:37:54 +0530

    i have set to button properly

--- src/Components/ViewBooks/ViewData.jsx
+++ src/Components/ViewBooks/ViewData.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import './Viewdata.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { DeleteAsync, EditDataAsync, GetDataAsync } from '../../Services/Actions/BooksActions';
@@ -10,12 +10,12 @@ function ViewData() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     // const { books } = useSelector(state => state.BookReducer)
-    const { books ,isEdit} = useSelector(state => state.BooksReducer)
+    const { books, isEdit } = useSelector(state => state.BooksReducer)
 
     const handleEdit = (id) => {
 
         dispatch(EditDataAsync(id))
-       
+
     }
     const handleDelet = (id) => {
         dispatch(DeleteAsync(id))
@@ -25,47 +25,48 @@ function ViewData() {
         dispatch(GetDataAsync());
     }, [])
 
-    useEffect(()=>{
-        if(isEdit){
+    useEffect(() => {
+        if (isEdit) {
             navigate('/edit')
         }
-    },[isEdit])
+    }, [isEdit])
     return (
         <>
             <Container fluid>
+                <Row>
+                    <table className="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th className='border border-dark text-center'>SR No</th>
+                                <th className='border border-dark text-center'>Title</th>
+                                <th className='border border-dark text-center'>Author</th>
+                                <th className='border border-dark text-center'>Genre</th>
+                                <th className='border border-dark text-center'>Year</th>
+                                <th className='border border-dark text-center'>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                books.map((data) => {
 
-                <table className="table table-success table-striped">
-                    <thead>
-                        <tr>
-                            <th className='border border-dark text-center'>SR No</th>
-                            <th className='border border-dark text-center'>Title</th>
-                            <th className='border border-dark text-center'>Author</th>
-                            <th className='border border-dark text-center'>Genre</th>
-                            <th className='border border-dark text-center'>Year</th>
-                            <th className='border border-dark text-center'>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            books.map((data) => {
-
-                                return (
-                                    <tr >
-                                        <td className='border border-dark text-center col-2'>{data.id}</td>
-                                        <td className='border border-dark text-center col-2'>{data.title}</td>
-                                        <td className='border border-dark text-center col-2'> {data.author}</td>
-                                        <td className='border border-dark text-center col-2'>{data.genre}</td>
-                                        <td className='border border-dark text-center col-2'>{data.year}</td>
-                                        <td className='d-flex justify-content-center gap-3 border-1 border-dark'>
-                                            <button type='submit' className='btn btn-success' onClick={() => handleEdit(data.id)}>Edit</button>
-                                            <button className='btn btn-danger' onClick={() => handleDelet(data.id)}>Delete</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
+                                    return (
+                                        <tr >
+                                            <td className='border border-dark text-center col-2'>{data.id}</td>
+                                            <td className='border border-dark text-center col-2'>{data.title}</td>
+                                            <td className='border border-dark text-center col-2'> {data.author}</td>
+                                            <td className='border border-dark text-center col-2'>{data.genre}</td>
+                                            <td className='border border-dark text-center col-2'>{data.year}</td>
+                                            <td className='d-flex justify-content-center gap-3 py-3 border-dark'>
+                                                <button type='submit' className='btn btn-success' onClick={() => handleEdit(data.id)}>Edit</button>
+                                                <button className='btn btn-danger' onClick={() => handleDelet(data.id)}>Delete</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </Row>
             </Container>
         </>
     );
